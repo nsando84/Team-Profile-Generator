aefe4a57714ee883a56e2e19ccbc559141ff52bc
nicksandoval <sand8val@gmail.com>
2020-09-05 16:51:11 -0700

    added to app.js

--- app.js
+++ app.js
@@ -1,35 +1,110 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+// const Manager = require("./lib/Manager");
+// const Engineer = require("./lib/Engineer");
+// const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+// const path = require("path");
+// const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+// const OUTPUT_DIR = path.resolve(__dirname, "output");
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+// const render = require("./lib/htmlRenderer");
+// const { inherits } = require("util");
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+class Mainloop {
+    constructor() {
+        
+    }
+    init() {
+        inquirer
+            .prompt([
+                {
+                    type: "list",
+                    name: "intChoice",
+                    message: "What would you like to do?",
+                    choices: [
+                        "Create new employee",
+                        "exit"
+                    ]
+                }
+             ])
+            .then(intChoice => {
+                intChoice.intChoice === "exit" ? this.runExit() : this.runNewEmployee()
+            })
+            .catch(function(err) {
+                console.error(err);
+            });
+    }
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+////////////////// RUN EXIT PROGRAM ////////////////////
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+    runExit() {
+        inquirer
+            .prompt([
+                {
+                    type: "list",
+                    name: "byeChoice",
+                    message: "Are you sure you want to exit?",
+                    choices: [
+                        "Yes",
+                        "No"
+                    ]
+                }
+            ])
+            .then(byeChoice => {
+                if (byeChoice.byeChoice === "Yes") { 
+                    console.log("GoodBye")
+                    process.exit(0) 
+                } else { 
+                    this.init() 
+                }     
+            })
+            .catch(function(err) {
+                console.error(err);
+            });
+    }  
+    
+////////////////// CREATE NEW EMPLOYEE ////////////////////
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+    runNewEmployee() {
+        inquirer
+            .prompt([
+                {
+                    type: "list",
+                    name: "employeeChoice",
+                    message: "Employee position",
+                    choices: [
+                        "Manager",
+                        "Engineer",
+                        "Intern",
+                        "<<<- Go back"
+                    ]
+                }
+            ])
+            .then(employeeChoice => {
+                switch (employeeChoice.employeeChoice) { 
+                    case "Manager":
+                        console.log("manager")
+                        break;
+                    case "Engineer":
+                        console.log("Engineer")
+                        break;
+                    case "Intern":
+                        console.log("Intern")
+                        break;
+                    default:
+                        this.init()
+                }     
+            })
+            .catch(function(err) {
+                console.error(err);
+            });
+    }  
+}
+
+
+const newInit = new Mainloop;
+newInit.init()
+// console.log(init.runExit())
