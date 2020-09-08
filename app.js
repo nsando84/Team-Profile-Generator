9ddfa4cc5e2dc519d2a3e27ab812fc3392493dd8
nicksandoval <sand8val@gmail.com>
2020-09-08 12:38:25 -0700

    refactored the employee setup into own file

--- app.js
+++ app.js
@@ -1,24 +1,8 @@
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-const Manager = require("./lib/Manager");
-const { runNewManager } = require("./promptLib/newManager")
-const { runNewEngineer } = require("./promptLib/newEngineer")
-const { runNewIntern } = require("./promptLib/newIntern")
-const path = require("path");
-const fs = require("fs");
-
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-
-const render = require("./lib/htmlRenderer");
-const { inherits } = require("util");
+const { runNewEmployee } = require("./promptLib/employeeSetup")
+const { runCheckEmployee } = require("./promptLib/employeeSetup")
 
 
-const EmployeeArr = []
-
-    
-// ////////////////// RUN START PROGRAM ////////////////////    
 
     init = () => {
         inquirer
@@ -56,9 +40,6 @@ const EmployeeArr = []
         
     }
 
-
-////////////////// RUN EXIT PROGRAM ////////////////////
-
     runExit = () => {
         inquirer
             .prompt([
@@ -86,142 +67,13 @@ const EmployeeArr = []
             });
     }  
     
-//////////////////// CREATE NEW EMPLOYEE ////////////////////
-
-    addEmployee = (newManager) => {
-        EmployeeArr.push(newManager)
-    }
-
-    runNewEmployee = () => {
-        inquirer
-            .prompt([
-                {
-                    type: "list",
-                    name: "employeeChoice",
-                    message: "Employee position",
-                    choices: [
-                        "Manager",
-                        "Engineer",
-                        "Intern",
-                        "<<<- Go back"
-                    ]
-                }
-            ])
-            .then(employeeChoice => {
-                switch (employeeChoice.employeeChoice) { 
-                    case "Manager":
-                        runNewManager()
-                        break;
-                    case "Engineer":
-                        runNewEngineer()
-                        break;
-                    case "Intern":
-                        runNewIntern()
-                        break;
-                    default:
-                        init()
-                }     
-            })
-            .catch(function(err) {
-                console.error(err);
-            });
-    }  
-
-//////////////////// Check EMPLOYEES ////////////////////
-
-    checkEmployeeChoices = () => {
-        EmployeeArr.length === 0 ? console.log("\x1b[31m","No employees") : console.log(EmployeeArr)
-    }
-   
-    
-
-    deleteEmployee = () => {
-        inquirer
-            .prompt([
-                {
-                    type: "list",
-                    message: "Pick employee to delete",
-                    name: "deleteEmployee",
-                    choices: [
-                        ...EmployeeArr,
-                        "<<<- Go back"
-                    ]
-                }
-            ]).then(deleteEmployee => {
-                switch (deleteEmployee.deleteEmployee) {
-                    case "<<<- Go back":
-                        runCheckEmployee()
-                        break;
-                    default:
-                        let deleteChoice = deleteEmployee.deleteEmployee
-                        EmployeeArr.forEach(function(e, i) {
-                            if (e.name === deleteChoice) {
-                                console.log("\x1b[33m", `${e.name} has been deleted`)
-                                EmployeeArr.splice(i, 1)
-                            }
-                        })
-                    init()
-                }
-            })
-    }
-
-    runCheckEmployee = () => {
-        inquirer
-            .prompt([
-                {
-                type: "list",
-                message: "Current Employees",
-                name: "currentEmployee",
-                choices: [
-                    "Check employees",
-                    "Delete employee",
-                    "Complete procress",
-                    "<<<- Go back"
-                    ]
-                }
-            ])
-            .then(currentEmployee => {
-                switch (currentEmployee.currentEmployee) {
-                    case "<<<- Go back":
-                        init()
-                        break;
-                    case "Check employees":
-                        checkEmployeeChoices(),
-                        init()
-                        break;
-                    case "Complete procress":
-                        runEmployeeComplete()
-                        break;
-                    default:    
-                        deleteEmployee()
-                }
-            })
-            .catch(function(err) {
-                console.error(err);
-            }); 
-    }
-
-//////////////// Complete Process FUNCTION ////////////////////
-
-    runEmployeeComplete = () => {
-        let htmlFile = render(EmployeeArr)
-        // outputPath
-        fs.writeFile(outputPath, htmlFile, function(err) {
-
-            if (err) {
-              return console.log(err);
-            }
-          
-            console.log("Success!");
-          
-          });
-        // console.log(render(EmployeeArr)) 
-    }
-    
-
-//////////////// Starter FUNCTION ////////////////////
 
 init()
+
+module.exports = {
+    init: init,
+    runExit: runExit
+}
 
 
 
